90192f2249a72414e643dfde053b61def30fba9f
Michaël Vanderheyden <michael.vanderheyden@eviden.com>
2025-03-20 10:26:21 +0100

    fix: update Windows cleanup script to handle hidden files and directories (#239)

--- test/add_files.ps1
+++ test/add_files.ps1
@@ -7,3 +7,7 @@ echo "insert text here" > ./myfolder/myfile.txt
 echo "insert text here" > ./myfolder/.myhiddenfile.txt
 echo "insert text here" > ./.myhiddenfolder/myfile.txt
 echo "insert text here" > .myhiddenfile.txt
+
+attrib +H .myhiddenfolder /S /D
+attrib +H .myhiddenfolder/* /S /D
+attrib +H .myhiddenfile.txt /S /D
